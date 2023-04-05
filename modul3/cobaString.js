aa323df8b0c6b430aecae89f4578ddb27a78197c
Alfina Fitriyani <alfinayani931@gmail.com>
2023-04-05 11:31:50 +0700

    string

---
+++ modul3/cobaString.js
@@ -0,0 +1,10 @@
+let greet = "Hello";
+let moreGreet = greet + greet ;
+console.log(moreGreet);
+
+/* output : HelloHello */
+
+const MyName = "Alfina";
+console.log(`Hello, my name is ${MyName}`);
+
+/* output : Hello, my name is Alfina. */
