b7d5ffd09a9c7674e4de1f6fd5b25fd8f095235e
Alfina Fitriyani <alfinayani931@gmail.com>
2023-04-12 13:57:48 +0700

    object

---
+++ modul4/coba1Object.js
@@ -0,0 +1,8 @@
+const user = {
+    firstname: "Alfina",
+    lastname: "Fitriyani",
+    umur: 18,
+    isjedi: true,
+};
+console.log(`halo, nama saya ${user.firstname} ${user.lastname}`);
+console.log(`umur saya ${user.umur}`);
---
+++ modul4/coba2Array.js
@@ -0,0 +1,2 @@
+let a = ["Manis",67.7,90, false,"program"];
+console.log(a);
